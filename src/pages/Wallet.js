eefe41b8c8081c3e5145aaab754c2e387142b2e7
BrenoBTrindade <brenokaminski@hotmail.com>
2022-05-19 19:37:07 -0300

    refatorando requisito 8

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -135,36 +135,40 @@ class Wallet extends React.Component {
         </form>
         <div className="table-wallet">
           <table className="table">
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-            {expenses.map((expense) => (
-              <tr key={ expense.id }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ Number(expense.value).toFixed(2) }</td>
-                <td>{ expense.exchangeRates[expense.currency].name }</td>
-                <td>
-                  { Number(expense.exchangeRates[expense.currency].ask)
-                    .toFixed(2) }
-
-                </td>
-                <td>
-                  { (Number(expense.value)
-              * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2) }
-                </td>
-                <td>Real</td>
+            <thead>
+              <tr>
+                <th>Descrição</th>
+                <th>Tag</th>
+                <th>Método de pagamento</th>
+                <th>Valor</th>
+                <th>Moeda</th>
+                <th>Câmbio utilizado</th>
+                <th>Valor convertido</th>
+                <th>Moeda de conversão</th>
+                <th>Editar/Excluir</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {expenses.map((expense) => (
+                <tr key={ expense.id }>
+                  <td>{ expense.description }</td>
+                  <td>{ expense.tag }</td>
+                  <td>{ expense.method }</td>
+                  <td>{ Number(expense.value).toFixed(2) }</td>
+                  <td>{ expense.exchangeRates[expense.currency].name }</td>
+                  <td>
+                    { Number(expense.exchangeRates[expense.currency].ask)
+                      .toFixed(2) }
+
+                  </td>
+                  <td>
+                    { (Number(expense.value)
+              * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2) }
+                  </td>
+                  <td>Real</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </main>
